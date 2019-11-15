3e134406ac6239563eaef916df506f643e063251
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-11-15 17:24:47 +0100

    Delete UGapEc_confLevel.m

--- Modules/UGapEc_confLevel.m
+++
@@ -1,5 +0,0 @@
-function confLevel = UGapEc_confLevel(c, b, NbrPlayArms, delta)
-    K = length(NbrPlayArms);
-    t = sum(NbrPlayArms);
-    confLevel = b.*sqrt(c.*log(4*K.*t.^3./delta)./NbrPlayArms);
-end
