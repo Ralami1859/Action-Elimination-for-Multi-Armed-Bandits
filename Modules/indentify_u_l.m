c9635a1737154903173e5b4ecafdf30be97997ac
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-11-15 17:25:21 +0100

    Delete indentify_u_l.m

--- Modules/indentify_u_l.m
+++
@@ -1,9 +0,0 @@
-function res = indentify_u_l(U, L, J)
-    [~, l_t] = min(L(J));
-    l_t = J(l_t);
-    J_bar = setdiff([1:length(U)], J);
-    [~, u_t] = max(L(J_bar));
-    u_t = J_bar(u_t);
-    res = [l_t, u_t];
-
-    
