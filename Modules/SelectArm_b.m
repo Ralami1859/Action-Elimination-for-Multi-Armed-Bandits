6ee5121072aee2e87827a1c45bec96f0fbd09003
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-11-15 17:25:47 +0100

    Delete SelectArm_b.m

--- Modules/SelectArm_b.m
+++
@@ -1,7 +0,0 @@
-function [I_t, J] = SelectArm_b(mu_hat, NbrPlayArms, expParam, b, m_arms)
-    [B, L, U, confLevel] = computeIndices_b(mu_hat, NbrPlayArms, expParam, b, m_arms);
-    [~, J] = min_set(B, 1, m_arms);
-    armTP_t = indentify_u_l(U, L, J);
-    [~, I_t] = max(confLevel(armTP_t));
-    I_t = armTP_t(I_t);
-    
