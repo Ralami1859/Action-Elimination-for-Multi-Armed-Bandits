83739997b2d3bb7d9080b8ca92584d6388fb9def
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-11-15 17:26:05 +0100

    Delete LUCB_updateConfBounds.m

--- Modules/LUCB_updateConfBounds.m
+++
@@ -1,5 +0,0 @@
-function [mu_hat, NbrPlayArms] = LUCB_updateConfBounds(l_u, reward, mu_hat, NbrPlayArms)
-
-mu_hat(l_u) = (mu_hat(l_u).*NbrPlayArms(l_u)+ reward)./NbrPlayArms(l_u);
-NbrPlayArms(l_u) = NbrPlayArms(l_u) + 1;
-
