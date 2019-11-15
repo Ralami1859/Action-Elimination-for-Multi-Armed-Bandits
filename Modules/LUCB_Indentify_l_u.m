38ae64ca3745fdb4f6183f7e7e903014fa7b4819
Reda ALAMI <37814001+Ralami1859@users.noreply.github.com>
2019-11-15 17:24:24 +0100

    Delete LUCB_Indentify_l_u.m

--- Modules/LUCB_Indentify_l_u.m
+++
@@ -1,4 +0,0 @@
-function [armTP_t,J,U,L, B] = LUCB_Indentify_l_u(mu_hat, NbrPlayArms, expParam, b, m_arms, delta)
-  [B, L, U] = computeIndices(mu_hat, NbrPlayArms, expParam, b, m_arms, delta);
-  [~, J] = min_set(B, 1, m_arms);
-  armTP_t = indentify_u_l(U, L, J);
